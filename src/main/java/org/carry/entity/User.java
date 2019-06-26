f9a5e9817dd248e7bbe564849486141327c6fe50
Carry <xkx@qq.com>
2019-06-26 09:22:06 +0800

    first commit

---
+++ src/main/java/org/carry/entity/User.java
@@ -0,0 +1,16 @@
+package org.carry.entity;
+
+import lombok.Data;
+
+/**
+ * @Author: CARRY
+ * @CreateTime: 2019-06-25 14:42
+ * @Description: ${Description}
+ */
+@Data
+public class User {
+    private Integer id;
+    private String userName;
+    private String passWord;
+    private String user_sex;
+}
